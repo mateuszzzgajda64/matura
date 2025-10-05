94a456e608c388e28244459473b03f94d3ac9729
mateuszzzgajda64 <mateuszzzgajda64@gmail.com>
2025-10-05 18:08:17 +0200

    Create zbiory_oznaczenia.py

---
+++ zbiory_oznaczenia.py
@@ -0,0 +1,15 @@
+zbior = {1 , 2 , 3 }
+zbior2 = {3 , 4 , 5}
+
+suam = zbior | zbior2
+print(suam)
+
+roznica = zbior - zbior2
+print(roznica)
+
+czescWspolna = zbior & zbior2
+print(czescWspolna)
+
+sumaBezCzesciWspolnej = zbior ^ zbior2
+print(sumaBezCzesciWspolnej)
+
