c8bb6736849d1d08d1b55fa3b3779890618b2143
mateuszzzgajda64 <mateuszzzgajda64@gmail.com>
2025-11-16 18:15:39 +0100

    Create napisy_szyfrowanie_przestawieniowe.py

---
+++ napisy_szyfrowanie_przestawieniowe.py
@@ -0,0 +1,14 @@
+napis = str(input())
+
+def szyfruj(slowo):
+    tab = list(slowo)
+    for i in range(0 , len(tab) -1 , 2):
+        tab[i] , tab[i + 1] = tab[i + 1], tab[i]
+    return tab
+
+wynik = ''
+slowo = szyfruj(napis)
+for i in range(len(slowo)):
+    wynik += slowo[i]
+
+print(wynik)
